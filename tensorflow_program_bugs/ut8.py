1b899a497c0209f80e371b7d7d47d675ee22eb1e
Matthew Rahtz <mrahtz@google.com>
2021-04-07 20:55:29 +0100

    Check what types are in UT8

--- tensorflow_program_bugs/ut8.py
+++ tensorflow_program_bugs/ut8.py
@@ -7,8 +7,11 @@ np.random.seed(20180130)
 
 data = np.array([0.1, 0.2])
 x = tf.placeholder("float", shape=(2,))
+reveal_type(x)
 T1 = tf.Variable(tf.ones((2, 2)))
+reveal_type(T1)
 l1 = tf.matmul(T1, x)
+reveal_type(l1)
 init = tf.initialize_all_variables()
 
 with tf.Session() as sess:

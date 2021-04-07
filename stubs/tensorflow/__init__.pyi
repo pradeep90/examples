d29b09172406a806b02e1cb188c600327c86d5b0
Matthew Rahtz <mrahtz@google.com>
2021-04-07 20:55:17 +0100

    Fix problems with TensorFlow stubs

--- stubs/tensorflow/__init__.pyi
+++ stubs/tensorflow/__init__.pyi
@@ -1,4 +1,4 @@
-from typing import Generic, Optional, Override, NoReturn, Tuple, TypeVar
+from typing import Any, Generic, Optional, overload, NoReturn, Tuple, TypeVar
 from pyre_extensions import TypeVarTuple
 
 Shape = TupleVarTuple('Shape')
@@ -6,56 +6,70 @@ A1 = TypeVar('A1')
 A2 = TypeVar('A2')
 A3 = TypeVar('A3')
 
+
 class Tensor(Generic[*Shape]):
-  ..
+  ...
+
 
 def placeholder(
     dtype,
     shape: Tuple[int],
-) -> Tensor[Any, Any]:
+) -> Tensor[Any]:
   ...
+
 
 def ones(
     shape: Tuple[int, int],
 ) -> Tensor[Any]:
   ...
 
+
 def Variable(
     initial_value: Tensor[A1],
 ) -> Tensor[A1]:
   ...
+
+
+class Session:
+
+    def __enter__(self) -> Session:
+        ...
+
+    def run(self, x: Tensor, feed_dict=None):
+        ...
  
+
 # ===== matul =====
 
-@override
+@overload
 def matmul(
     a: Tensor[A1],
     b: Tensor[A1],
 ) -> Tensor[A1]:
   ...
 
-@override
+@overload
 def matmul(
     a: Tensor[A1],
     b: Tensor[A1, A2],
 ) -> Tensor[A2]:
   ...
 
-@override
+@overload
 def matmul(
     a: Tensor[A1, A2],
     b: Tensor[A2],
 ) -> Tensor[A1]:
   ...
 
-@override
+@overload
 def matmul(
     a: Tensor[A1, A2],
     b: Tensor[A2, A3],
 ) -> Tensor[A1, A3]:
   ...
 
-@override
+@overload
 def matmul(
     a: Any,
     b: Any,

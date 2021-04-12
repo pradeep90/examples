303f6b84674579befb765352447c0b3e009c1629
Pradeep Kumar Srinivasan <pradeepkumars@fb.com>
2021-04-11 19:33:07 -0700

    numpy: make ndarray generic in DType.

--- stubs/numpy/__init__.pyi
+++ stubs/numpy/__init__.pyi
@@ -13,30 +13,33 @@ from typing import (
     Optional,
     Tuple,
     Type,
+    TypeVar,
     Union,
 )
 
 from pyre_extensions import TypeVarTuple, Unpack
 
+DType = TypeVar("DType")
 Ts = TypeVarTuple("Ts")
 Ts2 = TypeVarTuple("Ts2")
 
 _Shape = Tuple[Unpack[Ts]]
 
 class _ArrayOrScalarCommon(
-    Generic[Unpack[Ts]],
+    Generic[DType, Unpack[Ts]],
     SupportsInt,
     SupportsFloat,
     SupportsComplex,
     SupportsBytes,
     SupportsAbs[Any],
 ): ...
+class float: ...
 
-class ndarray(_ArrayOrScalarCommon[Unpack[Ts]], Iterable, Sized, Container):
+class ndarray(_ArrayOrScalarCommon[DType, Unpack[Ts]], Iterable, Sized, Container):
     def __init__(
         self,
         shape: Tuple[Unpack[Ts]],
-        dtype=...,
+        dtype: Type[DType] = ...,
         buffer=...,
         offset: Optional[int] = ...,
         strides: Tuple[int, ...] = ...,
@@ -46,22 +49,22 @@ class ndarray(_ArrayOrScalarCommon[Unpack[Ts]], Iterable, Sized, Container):
     @property
     def shape(self) -> Tuple[Unpack[Ts]]: ...
     @overload
-    def reshape(self, shape: Tuple[Unpack[Ts2]]) -> ndarray[Unpack[Ts2]]: ...
+    def reshape(self, shape: Tuple[Unpack[Ts2]]) -> ndarray[DType, Unpack[Ts2]]: ...
     @overload
-    def reshape(self, *shape: Unpack[Ts2]) -> ndarray[Unpack[Ts2]]: ...
-    def __add__(self, other) -> ndarray[Unpack[Ts]]: ...
-    def __div__(self, other) -> ndarray[Unpack[Ts]]: ...
-    def __truediv__(self, other) -> ndarray[Unpack[Ts]]: ...
-    def astype(self, dtype: str) -> ndarray[Unpack[Ts]]: ...
+    def reshape(self, *shape: Unpack[Ts2]) -> ndarray[DType, Unpack[Ts2]]: ...
+    def __add__(self, other) -> ndarray[DType, Unpack[Ts]]: ...
+    def __div__(self, other) -> ndarray[DType, Unpack[Ts]]: ...
+    def __truediv__(self, other) -> ndarray[DType, Unpack[Ts]]: ...
+    def astype(self, dtype: str) -> ndarray[DType, Unpack[Ts]]: ...
 
 def empty(
-    shape: Union[int, Tuple[Unpack[Ts]]], dtype: Union[Type[Any], str]
-) -> ndarray[Unpack[Ts]]: ...
+    shape: Union[int, Tuple[Unpack[Ts]]], dtype: Union[Type[DType], str]
+) -> ndarray[DType, Unpack[Ts]]: ...
 def array(
     object: object,
-    dtype=...,
+    dtype: Type[DType] = ...,
     copy: bool = ...,
     subok: bool = ...,
     ndmin: int = ...,
-) -> ndarray[Unpack[Tuple[Any, ...]]]: ...
-def sin(x: ndarray[Unpack[Ts]]) -> ndarray[Unpack[Ts]]: ...
+) -> ndarray[DType, Unpack[Tuple[Any, ...]]]: ...
+def sin(x: ndarray[DType, Unpack[Ts]]) -> ndarray[DType, Unpack[Ts]]: ...
--- stubs/tensorflow/__init__.pyi
+++ stubs/tensorflow/__init__.pyi
@@ -21,6 +21,7 @@ from numpy import ndarray
 
 Ts = TypeVarTuple("Ts")
 T = TypeVar("T")
+DType = TypeVar("DType")
 
 A1 = TypeVar("A1")
 A2 = TypeVar("A2")
@@ -28,17 +29,17 @@ A3 = TypeVar("A3")
 A4 = TypeVar("A4")
 A5 = TypeVar("A5")
 
-class Variable(Generic[Unpack[Ts]]):
+class Variable(Generic[DType, Unpack[Ts]]):
     shape: Tuple[Unpack[Ts]]
     def __init__(
         self,
-        initial_value: ndarray[Unpack[Ts]] = ...,
+        initial_value: ndarray[DType, Unpack[Ts]] = ...,
         trainable=...,
         validate_shape=...,
         caching_device=...,
         name=...,
         variable_def=...,
-        dtype=...,
+        dtype: Type[DType] = ...,
         import_scope=...,
         constraint=...,
         synchronization=...,
@@ -52,9 +53,9 @@ seed: Any = ...
 
 class float32: ...
 
-class Tensor(Generic[T, Unpack[Ts]]):
+class Tensor(Generic[DType, Unpack[Ts]]):
     shape: Tuple[Unpack[Ts]]
-    def __eq__(self, other: Tensor[T, Unpack[Ts]]) -> bool: ...
+    def __eq__(self, other: Tensor[DType, Unpack[Ts]]) -> bool: ...
 
 def matmul(
     a,

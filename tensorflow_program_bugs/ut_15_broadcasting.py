cfa310e7fb673d48f12c53e945cb9dcdf64d02be
Matthew Rahtz <mrahtz@google.com>
2021-04-17 21:26:53 +0100

    Convert UT-15 to TF2

--- tensorflow_program_bugs/ut_15_broadcasting.py
+++ tensorflow_program_bugs/ut_15_broadcasting.py
@@ -1,11 +1,9 @@
 import tensorflow as tf
 
-assert tf.__version__ == "1.8.0"
-tf.set_random_seed(20180130)
 
 def weight_variable(shape):
     """Initialize the weights with random weights"""
-    initial = tf.truncated_normal(shape, stddev=0.1, dtype=tf.float64)
+    initial = tf.random.truncated_normal(shape, stddev=0.1, dtype=tf.float64)
     return tf.Variable(initial)
 
 
@@ -13,23 +11,32 @@ def weight_variable(shape):
 x = tf.constant([[1.0, 1.0], [1.0, 2.0], [1.0, 3.0]], dtype=tf.float64)
 y_ = tf.constant([1.0, 2.0, 3.0], dtype=tf.float64)
 w = weight_variable((2, 1))
-y = tf.matmul(x, w)
 
-error = tf.reduce_mean(tf.square(y_ - y))
+def compute_predictions():
+    y = tf.matmul(x, w)
+    return y
 
-train_step = tf.train.AdamOptimizer(1e-4).minimize(error)
+def compute_error():
+    y = compute_predictions()
+    error = tf.reduce_mean(tf.square(y_ - y))
+    return error
 
-with tf.Session() as sess:
-    sess.run(tf.initialize_all_variables())
+optimizer = tf.keras.optimizers.Adam(1e-4)
 
-    # Train the model and output every 1000 iterations
-    for i in range(50000):
-        sess.run(train_step)
-        err = sess.run(error)
+def train_step():
+    with tf.GradientTape() as tape:
+        error = compute_error()
+    grads = tape.gradient(error, (w,))
+    optimizer.apply_gradients(zip(grads, (w,)))
 
-        if i % 1000 == 0:
-            print("\nerr:", err)
-            print("x: ", sess.run(x))
-            print("w: ", sess.run(w))
-            print("y_: ", sess.run(y_))
-            print("y: ", sess.run(y))
+# Train the model and output every 1000 iterations
+for i in range(50000):
+    train_step()
+    err = compute_error()
+
+    if i % 1000 == 0:
+        print("\nerr:", err)
+        print("x: ", x)
+        print("w: ", w)
+        print("y_: ", y_)
+        print("y: ", compute_predictions())

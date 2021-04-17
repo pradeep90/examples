84cc7f5479a3d34d6caeaa74a5b0aa748a3ec5a7
Matthew Rahtz <mrahtz@google.com>
2021-04-17 21:45:18 +0100

    Add stub for `tf.random.uniform`

--- stubs/tensorflow/random.pyi
+++ stubs/tensorflow/random.pyi
@@ -6,3 +6,11 @@ Ts = TypeVarTuple("Ts")
 DType = TypeVar("DType")
 
 def truncated_normal(shape: Tuple[Unpack[Ts]], stddev: float) -> Tensor[float32, Unpack[Ts]]: ...
+def uniform(
+    shape: Tuple[Unpack[Ts]],
+    minval: Optional[Union[int, float]] = ...,
+    maxval: Optional[Union[int, float]] = ...,
+    dtype: Optional[Any] = ...,
+    seed: Optional[int] = ...,
+    name: Optional[str] = ...,
+) -> Tensor[float32, Unpack[Ts]]: ...

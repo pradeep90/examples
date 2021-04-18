fast_neural_style: fix formula; identify bug in Pyre type arithmetic.
+ Make `Padding` bound by int.
+ Note down confusing error messages.

fast_neural_style/neural_style/transformer_net.py
@@ -46,7 +46,7 @@ class TransformerNet(Module):
         # Non-linearities
         self.relu = ReLU()
 
-    def forward(self, X: Tensor[float, L[1], L[3], L[1080], L[1080]]) -> Tensor[float, L[1], L[3], L[1060], L[1060]]:
+    def forward(self, X: Tensor[float, L[1], L[3], L[1080], L[1080]]) -> Tensor[float, L[1], L[3], L[1080], L[1080]]:
         y1 = self.relu(self.in1(self.conv1(X)))
         y2 = self.relu(self.in2(self.conv2(y1)))
         y3 = self.relu(self.in3(self.conv3(y2)))
@@ -87,10 +87,8 @@ class ConvLayer(Module, Generic[InChannels, OutChannels, KernelSize, Stride]):
         DType,
         Batch,
         OutChannels,
-        # (height - kernel_size + 2 * padding) / stride + 1, where padding=0 by default
-        Add[Divide[Add[Height, Multiply[KernelSize, L[-1]]], Stride], L[1]],
-        # (width - kernel_size + 2 * padding) / stride + 1, where padding=0 by default
-        Add[Divide[Add[Width, Multiply[KernelSize, L[-1]]], Stride], L[1]],
+        Add[Divide[Add[Add[Height, Multiply[KernelSize, L[-1]]], Multiply[Divide[KernelSize, L[2]], L[2]]], Stride], L[1]],
+        Add[Divide[Add[Add[Width, Multiply[KernelSize, L[-1]]], Multiply[Divide[KernelSize, L[2]], L[2]]], Stride], L[1]],
     ]:
         return super()(x)
 
@@ -98,10 +96,8 @@ class ConvLayer(Module, Generic[InChannels, OutChannels, KernelSize, Stride]):
         DType,
         Batch,
         OutChannels,
-        # (height - kernel_size + 2 * padding) / stride + 1, where padding=0 by default
-        Add[Divide[Add[Height, Multiply[KernelSize, L[-1]]], Stride], L[1]],
-        # (width - kernel_size + 2 * padding) / stride + 1, where padding=0 by default
-        Add[Divide[Add[Width, Multiply[KernelSize, L[-1]]], Stride], L[1]],
+        Add[Divide[Add[Add[Height, Multiply[KernelSize, L[-1]]], Multiply[Divide[KernelSize, L[2]], L[2]]], Stride], L[1]],
+        Add[Divide[Add[Add[Width, Multiply[KernelSize, L[-1]]], Multiply[Divide[KernelSize, L[2]], L[2]]], Stride], L[1]],
     ]:
         out = self.reflection_pad(x)
         out = self.conv2d(out)
@@ -134,19 +130,10 @@ class ResidualBlock(Module, Generic[Channels]):
         residual = x
         out = self.relu(self.in1(self.conv1(x)))
         out = self.in2(self.conv2(out))
-
-        # Batch x Channels x Height-4 x Width-4
-        # reveal_type(out)
-        # Batch x Channels x Height x Width
-        # reveal_type(residual)
-
-        # Not sure how this is not a runtime error.
-        # REPL:
-        # >>> torch.zeros(1, 2, 10, 10) + torch.zeros(1, 2, 6, 6)
-        # RuntimeError: The size of tensor a (10) must match the size of
-        # tensor b (6) at non-singleton dimension 3
-        out = out + residual  # type: ignore
-        return out  # type: ignore
+        # pyre-ignore[58]: Bug in Pyre where N + 1 - 1 is not treated as compatible with N.
+        out = out + residual
+        # pyre-ignore[7]: Bug in Pyre where N + 1 - 1 is not treated as compatible with N.
+        return out
 
 
 class UpsampleConvLayer(Module, Generic[InChannels, OutChannels, KernelSize, Stride, Upscale]):
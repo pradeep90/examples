a29cfa32b5471c3bce742549cdb4bd6442c53b28
Matthew Rahtz <mrahtz@google.com>
2021-04-18 12:13:51 +0100

    Convert UT-1 to TF2

--- tensorflow_program_bugs/ut_1.py
+++ tensorflow_program_bugs/ut_1.py
@@ -1,15 +1,10 @@
-from tensorflow.examples.tutorials.mnist import input_data
-
-mnist = input_data.read_data_sets("/tmp/data", one_hot=True)
-
+import numpy as np
 import tensorflow as tf
-
-assert tf.__version__ == "1.8.0"
-tf.set_random_seed(20180130)
+import tensorflow_datasets as tfds
 
 
 def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
+    initial = tf.random.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
 
 
@@ -27,51 +22,81 @@ def max_pool_2x2(x):
                           strides=[1, 2, 2, 1], padding='SAME')
 
 
-sess = tf.InteractiveSession()
-
-x = tf.placeholder(tf.float32, shape=[None, 784])
-y_ = tf.placeholder(tf.float32, shape=[None, 10])
-
 W_conv1 = weight_variable([5, 5, 1, 32])
 b_conv1 = bias_variable([32])
-
-x_image = tf.reshape(x, [-1, 28, 28, 1])
-
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
-
 W_conv2 = weight_variable([5, 5, 32, 64])
 b_conv2 = bias_variable([64])
-
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
-
 W_fc1 = weight_variable([7 * 7 * 64, 1024])
 b_fc1 = bias_variable([1024])
-
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
-keep_prob = tf.placeholder(tf.float32)
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
 W_fc2 = weight_variable([1024, 10])
 b_fc2 = bias_variable([10])
+parameters = (W_conv1, b_conv1, W_conv2, b_conv2, W_fc1, b_fc1, W_fc2, b_fc2)
 
-y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
-cross_entropy = -tf.reduce_sum(y_ * tf.log(tf.clip_by_value(y_conv, 1e-10, 1.0)))
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-sess.run(tf.initialize_all_variables())
-for i in range(20000):
-    batch = mnist.train.next_batch(50)
+def compute_prediction(x, keep_prob):
+    h_conv1 = tf.nn.relu(conv2d(x, W_conv1) + b_conv1)
+    h_pool1 = max_pool_2x2(h_conv1)
+
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_pool2 = max_pool_2x2(h_conv2)
+
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+    h_fc1_drop = tf.nn.dropout(h_fc1, rate=(1-keep_prob))
+
+    y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+    return y_conv
+
+
+def compute_loss(x, y_, keep_prob):
+    y_conv = compute_prediction(x, keep_prob)
+    y_onehot = tf.one_hot(y_, depth=10)
+    cross_entropy = -tf.reduce_sum(y_onehot * tf.math.log(tf.clip_by_value(y_conv, 1e-10, 1.0)))
+    return cross_entropy
+
+
+optimizer = tf.keras.optimizers.Adam(1e-4)
+
+
+def train_step(x, y_, keep_prob):
+    with tf.GradientTape() as tape:
+        cross_entropy = compute_loss(x, y_, keep_prob)
+    grads = tape.gradient(cross_entropy, parameters)
+    optimizer.apply_gradients(zip(grads, parameters))
+
+
+def compute_accuracy(x, y_, keep_prob):
+    y_conv = compute_prediction(x, keep_prob)
+    correct_prediction = tf.equal(tf.argmax(y_conv, 1), y_)
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    return accuracy
+
+
+def load_dataset(split):
+    dataset = tfds.load(name="mnist", split=split)
+    def normalize(example):
+        example['image'] = tf.cast(example['image'], tf.float32) / 255.0
+        return example
+    dataset = dataset.map(normalize)
+    return dataset
+
+
+train_dataset = load_dataset(tfds.Split.TRAIN)
+train_dataset = train_dataset.batch(50)
+train_dataset = train_dataset.repeat()
+train_dataset_iterator = iter(train_dataset)
+for i in range(1000):
+    batch = next(train_dataset_iterator)
     if i % 100 == 0:
-        loss, train_accuracy = sess.run([cross_entropy, accuracy], feed_dict={
-            x: batch[0], y_: batch[1], keep_prob: 1.0})
+        loss = compute_loss(x=batch['image'], y_=batch['label'], keep_prob=1.0)
+        train_accuracy = compute_accuracy(x=batch['image'], y_=batch['label'], keep_prob=1.0)
         print("step %d, training accuracy %g, loss %g" % (i, train_accuracy, loss))
-    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+    train_step(x=batch['image'], y_=batch['label'], keep_prob=0.5)
 
-print("test accuracy %g" % accuracy.eval(feed_dict={
-    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+test_dataset = load_dataset(tfds.Split.TEST)
+test_tuples = ((x['image'], x['label']) for x in test_dataset)
+test_images, test_labels = zip(*test_tuples)
+test_images = np.array(test_images)
+test_labels = np.array(test_labels)
+print("test accuracy %g" % compute_accuracy(x=test_images, y_=test_labels, keep_prob=1.0))
